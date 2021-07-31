dc15cbef1ccedf5d2b75f8d1b2be44ac9bf9f84d
kurodamaria <xfreunion@gmail.com>
2021-07-31 21:45:39 +0800

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../doc/*.stories.mdx",
+    "../src/components/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
