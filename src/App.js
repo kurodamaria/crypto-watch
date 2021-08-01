37c28adbf45142a2f7b3ba6045bd6915c7cf1f1f
kurodamaria <xfreunion@gmail.com>
2021-08-01 19:12:40 +0800

    switch to coingecko api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState, memo } from 'react'
 import styled from 'styled-components'
+import CoinGecko from 'coingecko-api'
+
+const CoinGeckoClient = new CoinGecko()
 
 const Container = styled.div`
   max-width: 1080px;
@@ -31,7 +34,7 @@ const RecordWrapper = styled.div`
   transition: all 0.3s;
 `
 
-const RecordIcon = styled.img`
+const RecordCoinIcon = styled.img`
   width: 1.5em;
   height: 1.5em;
 `
@@ -78,6 +81,14 @@ const Loading = () =>
     <Spiner radius='5em' borderRadius='5px' color='white' />
   </LoadingContainer>
 
+const DoFollow = styled.a.attrs({ target: 'blank' })`
+  text-decoration: underline dashed;
+  color: lightseagreen;
+  font-size: 0.6em;
+  display: block;
+  text-align: center;
+`
+
 // eslint-disable-next-line
 function useSize () {
   const ref = useRef()
@@ -104,12 +115,16 @@ function useSize () {
   return [ref, size]
 }
 
-const CryptoCoinPriceRecordItem = memo(({ logo_url, name, price }) => {
+const CryptoCoinPriceRecordItem = memo((coinData) => {
   return (
     <RecordWrapper>
-      <RecordIcon src={logo_url} />
-      <RecordCoinSymbol>{name}</RecordCoinSymbol>
-      <RecordCoinPrice>{price}</RecordCoinPrice>
+      <RecordCoinIcon src={coinData.image.small} />
+      <RecordCoinSymbol>
+        {coinData.name}
+      </RecordCoinSymbol>
+      <RecordCoinPrice>
+        ${coinData.market_data.current_price.usd}
+      </RecordCoinPrice>
     </RecordWrapper>
   )
 })
@@ -126,11 +141,7 @@ const MainPage = ({ cryptoData }) => {
       </div>
       <div>
         {
-        cryptoData && cryptoData.sort(
-          (a, b) => b.price - a.price
-        ).filter(
-          (crypto) => crypto.name.toLowerCase().includes(filter.toLowerCase())
-        ).map((crypto) => <CryptoCoinPriceRecordItem {...crypto} key={crypto.id} />)
+        cryptoData && cryptoData.map((crypto) => <CryptoCoinPriceRecordItem {...crypto} key={crypto.id} />)
       }
       </div>
     </>
@@ -140,14 +151,10 @@ const MainPage = ({ cryptoData }) => {
 function App () {
   const [cryptoData, setCryptoData] = useState(undefined)
   useEffect(() => {
-    fetch(
-      'https://api.nomics.com/v1/currencies/ticker?key=5582925c69cc16b163f51399df72b98bd7423284&interval=1d,30d&convert=EUR&per-page=100&page=1',
-      {
-        mode: 'cors'
-      }
-    )
-      .then(response => response.json())
-      .then(setCryptoData)
+    CoinGeckoClient.coins.all()
+      .then(data => {
+        setCryptoData(data.data)
+      })
   }, [])
 
   return (
@@ -157,6 +164,9 @@ function App () {
           ? <MainPage cryptoData={cryptoData} />
           : <Loading />
       }
+      <DoFollow href='https://nomics.com'>
+        Crypto Market Cap &amp; Pricing Data Provided By Nomics
+      </DoFollow>
     </Container>
   )
 }
